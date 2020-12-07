8320478db2e761e1c1bb0591ca451ac91632e240
oviya29 <venkatesanoviya@gmail.com>
2020-12-07 11:16:19 +0530

    all exerise program files added

---
+++ C/excercise/Basic_algorithm_exercise/EX1.c
@@ -0,0 +1,8 @@
+/*
+1. Write a C program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum. Go to the editor
+Expected Output:
+
+3
+12
+*/
+
---
+++ C/excercise/numbers_exercise/EX1.c
@@ -0,0 +1,56 @@
+/*1. Write a program in C to check whether a given number is an ugly number or not. Go to the editor
+
+Expected Output :
+Input an integer number: 25
+It is an ugly number.
+*/
+
+// CPP program to find nth ugly number 
+# include<stdio.h> 
+# include<stdlib.h> 
+  
+/*This function divides a by greatest divisible  
+  power of b*/
+int maxDivide(int a, int b) 
+{ 
+  while (a%b == 0) 
+   a = a/b;  
+  return a; 
+}     
+  
+/* Function to check if a number is ugly or not */
+int isUgly(int no) 
+{ 
+  no = maxDivide(no, 2); 
+  no = maxDivide(no, 3); 
+  no = maxDivide(no, 5); 
+    
+  return (no == 1)? 1 : 0; 
+}     
+  
+/* Function to get the nth ugly number*/
+int getNthUglyNo(int n) 
+{ 
+  int i = 1;  
+  int count = 1;   /* ugly number count */ 
+  
+  /*Check for all integers untill ugly count  
+    becomes n*/ 
+  while (n > count) 
+  { 
+    i++;       
+    if (isUgly(i)) 
+      count++;  
+  } 
+  return i; 
+} 
+  
+/* Driver program to test above functions */
+int main() 
+{ 
+    unsigned no = getNthUglyNo(150); 
+    printf("150th ugly no. is %d ",  no); 
+    getchar(); 
+    return 0; 
+} 
+
